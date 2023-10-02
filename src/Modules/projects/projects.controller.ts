e790f78ea588018dbd1ce14fd0f6f16db60af61e
Muhammad-Jawwad <muhammadjawwad417@gmail.com>
2023-10-02 01:47:40 -0700

    Few naming conventions updated

--- src/Modules/projects/projects.controller.ts
+++ src/Modules/projects/projects.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, UsePipes, ValidationPipe } from '@nestjs/common';
-import { CreateProjectDTO } from './DTO/CreateProject.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { ProjectsService } from './Services/projects.service';
+import { CreateProjectDTO } from '../Projects/DTO/CreateProject.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { ProjectsService } from './Services/projects.service';
+import { retry } from 'rxjs';
 import { UpdateProjectDTO } from './DTO/UpdateProject.dto';
 
 
 @Controller('projects')
 @ApiTags('Projects')
+
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post("create")
-  @UsePipes(ValidationPipe)
   async create(@Body() createProjectDto: CreateProjectDTO) {
     return await this.projectsService.create(createProjectDto);
     // return "project"
@@ -28,14 +29,15 @@ export class ProjectsController {
     return await this.projectsService.findOne(id);
   }
 
+
   @UseGuards(AuthGuard("jwt")) //Make a guard to filter every users projects for them 
   @Get("getProjectsByUser/:id")
   async findByUser(@Param('id') userId: string ){
    return await this.projectsService.getUsersProject(userId)
   }
 
+
   @Patch('update/:id')
-  @UsePipes(ValidationPipe)
   async update(@Param('id') id: string, @Body() updateProjectDto: UpdateProjectDTO) {
     return await this.projectsService.update(id, updateProjectDto);
   }
